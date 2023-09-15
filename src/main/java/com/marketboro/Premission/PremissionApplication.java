db17c9318b4899756ea5f46dd554d521fbd7cbb4
박윤찬 <dbscks9793@gmail.com>
2023-09-15 17:10:21 +0900

    Add: 프로젝트 초기설정

---
+++ src/main/java/com/marketboro/Premission/PremissionApplication.java
@@ -0,0 +1,13 @@
+package com.marketboro.Premission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PremissionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PremissionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marketboro/Premission/PremissionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marketboro.Premission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PremissionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
